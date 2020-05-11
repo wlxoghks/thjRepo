93d40f7bf4052f065a82982e646e9cd28bbc6474
DATA8320-21 <DATA8320-21@DESKTOP-23ALIC1>
2020-05-11 15:49:46 +0900

    기초 for문

---
+++ JAVAPrj/src/java5.java
@@ -0,0 +1,19 @@
+
+public class java5 {
+
+	public static void main(String[] args) {
+		int i;
+		for (i =0; i <3; i++)
+		{
+			System.out.printf("안녕하세요?\n");
+			System.out.printf("##또 안녕하세요?##\n");
+		}
+
+		System.out.printf("\n\n");
+		
+		for (i = 0; i < 3; i++)
+		System.out.printf("안녕하세요?\n");
+		System.out.printf("##또 안녕하세요?##\n");
+	}
+
+}
