7f6b5748ff7dd403b0af1b9729fd7dcc748ad6d5
DATA8320-21 <DATA8320-21@DESKTOP-23ALIC1>
2020-05-11 15:03:12 +0900

    조건문

---
+++ JAVAPrj/src/java4.java
@@ -0,0 +1,14 @@
+
+public class java4 {
+
+	public static void main(String[] args) {
+		int a = 200;
+		
+		if (a<100) {
+			System.out.printf("100보다 작군요..\n");
+		System.out.printf("거짓이므로 앞의 문장은 안보이겠죠?\n");
+		}		
+        System.out.printf("프로그램 끝!!\n");
+	}
+
+}
