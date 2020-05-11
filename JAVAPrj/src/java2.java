70f125f5b31ef31fdf09df721d9e059383f127c8
DATA8320-21 <DATA8320-21@DESKTOP-23ALIC1>
2020-05-11 13:30:26 +0900

    2주차

---
+++ JAVAPrj/src/java2.java
@@ -0,0 +1,11 @@
+
+public class java2 {
+
+	public static void main(String[] args) {
+		System.out.printf("%d+ %f = %f \n", 100, 10.3, 110.3);
+		
+		System.out.printf("%d/ %f = %d \n", 100, 10.3, 9);
+
+	}
+
+}
