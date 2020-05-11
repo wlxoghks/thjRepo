16dc653345bcb5bb465d59d8af555a0a66932f23
DATA8320-21 <DATA8320-21@DESKTOP-23ALIC1>
2020-05-11 15:53:23 +0900

    기타 풔 문

---
+++ JAVAPrj/src/java6.java
@@ -0,0 +1,12 @@
+
+public class java6 {
+
+	public static void main(String[] args) {
+		int i, k;
+		
+		for(i = 1, k=1; i <=9; i++, k++)
+			System.out.printf(" %d X %d = %d \n", i, k, i*k);
+
+	}
+
+}
