dbc412ca51bed45813679350800ef1f76d3a8c22
DATA8320-21 <DATA8320-21@DESKTOP-23ALIC1>
2020-05-11 14:46:27 +0900

    3주차

---
+++ JAVAPrj/src/java3.java
@@ -0,0 +1,15 @@
+
+public class java3 {
+
+	public static void main(String[] args) {
+		int a = 10, b;
+		
+
+		b = a++;
+		System.out.printf(" %d \n", b);
+		
+		b = ++a;
+		System.out.printf(" %d \n", b);
+	}
+
+}
