fd58e059adc71ee1595113703a83516e1e45b16d
DATA8320-21 <DATA8320-21@DESKTOP-23ALIC1>
2020-05-11 16:11:01 +0900

    별짓기

---
+++ JAVAPrj/src/java7.java
@@ -0,0 +1,18 @@
+
+public class java7 {
+
+	public static void main(String[] args) {
+		for (int i=1; i<=5; i++) {
+for (int j=1; j<=i; j++ ) { System.out.printf("*");}
+	
+		System.out.println();
+		}
+		
+		
+		for (int i=1; i<=4; i++) {
+for (int j=i; j<=4; j++) { 
+	System.out.printf("*");
+		} System.out.println(); 
+		}
+}
+}
