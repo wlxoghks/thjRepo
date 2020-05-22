766a9594029faf01a1fde17f03e2ab0a0ea2864d
DATA8320-21 <DATA8320-21@DESKTOP-23ALIC1>
2020-05-22 11:14:00 +0900

    배열의 크기 계산

---
+++ JAVAPrj/src/qodufdmlzmrlrPtks.java
@@ -0,0 +1,16 @@
+
+public class qodufdmlzmrlrPtks {
+
+	public static void main(String[] args) {
+		int aa[] = {10, 20, 30, 40, 50} ;
+		int count, size;
+		
+		count = aa.length;
+		size = count * Integer.BYTES;
+		
+		System.out.println("배열의 aa[]의 요소의 개수는" + count+"개입니다.");
+		System.out.println("배열의 aa[]의 요소의 개수는" + size+"바이트입니다.");
+
+	}
+
+}
