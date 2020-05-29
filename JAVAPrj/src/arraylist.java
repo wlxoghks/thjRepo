c0fa84689b8f2534fe1ab3cdbcea717fcce8fa32
DATA8320-21 <DATA8320-21@DESKTOP-23ALIC1>
2020-05-29 11:30:57 +0900

    ArrayList 예제 2 for, while문

--- JAVAPrj/src/arraylist.java
+++ JAVAPrj/src/arraylist.java
@@ -1,5 +1,6 @@
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class arraylist {
 
@@ -9,15 +10,24 @@ public class arraylist {
 
 		//List 컬렉션 프레임워크에 데이터를 추가함
 		rList.add("지태환");
-		rList.add("홍길동");
-		rList.add("고길동");
-		rList.add("임꺽정");
+		rList.add("태환 지");
+		rList.add("wlxoghks");
+		rList.add("thj");
 		
 		//배열크기 출력
-		System.out.println("배열의 크기 "+ rList.size());
+		//배열의 크기를 알기 때문에 while이아니라 for문을 사용
+ 		System.out.println("배열의 크기 "+ rList.size());
 		
 		for (int i=0; i<rList.size(); i++) {
 			System.out.println("["+i+"]번쨰 배열 기억공간 : "+rList.get(i));
+		}
+	
+		//List컬렉션을 통해 데이터를 가져올 때 for문을 쓰지 않고, while문을 쓰는것을 권장함
+		Iterator<String> it = rList.iterator();
+		while (it.hasNext()) {
+		String name = (String) it.next();
+		
+		System.out.println("이름 : " +name);
 		}
 	}
 
