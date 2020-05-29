e9857e7aca72b6d1afab51f2b1f41059dcacc1c5
DATA8320-21 <DATA8320-21@DESKTOP-23ALIC1>
2020-05-29 09:42:11 +0900

    쪽지시험

---
+++ JAVAPrj/src/Whrwltlgjaaa.java
@@ -0,0 +1,21 @@
+
+public class Whrwltlgjaaa {
+
+	public static void main(String[] args) {
+		int i=0;
+		int hap=0;
+		int num[]=new int[90];
+		
+		for(i=0; i<=90; i++) {
+			num[i]=(1+10)+1;
+			hap= hap+num[i];
+		}
+i=0;
+while(i<90) {
+	System.out.println(num[i]);
+	i++;
+			}
+	System.out.println(hap);
+	}
+
+}
